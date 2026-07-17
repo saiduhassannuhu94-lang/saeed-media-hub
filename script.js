b97900c4a318fd2111b9ac68ce8ef5da67728446
saiduhassannuhu94-lang <saiduhassannuhu94@gmail.com>
2026-07-17 17:39:00 +0100

    Implement loader functionality on page load
    Add a loader that disappears after 1 second

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
+window.addEventListener("load", () => {
 
+const loader = document.getElementById("loader");
+
+setTimeout(() => {
+
+loader.style.opacity = "0";
+
+setTimeout(() => {
+
+loader.style.display = "none";
+
+}, 600);
+
+}, 1000);
+
+});
