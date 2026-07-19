9ed97525c466b3a0228cf20059e72e6a3350fb2d
saiduhassannuhu94-lang <saiduhassannuhu94@gmail.com>
2026-07-19 12:13:34 +0100

    Remove menu toggle functionality from script.js

--- script.js
+++ script.js
@@ -73,9 +73,3 @@ window.addEventListener("scroll", () => {
     });
 
 });
-const menuToggle = document.getElementById("menu-toggle");
-const navLinks = document.querySelector(".nav-links");
-
-menuToggle.addEventListener("click", () => {
-    navLinks.classList.toggle("active");
-});
